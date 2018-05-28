26718b15d11cdfa5a2dc9eb5aa9e677cab51e779
Kevin Chi <kevinchi118@gmail.com>
2018-05-28 02:53:12 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -19,5 +19,13 @@ function lastImageSelector() {
 }
 
 function ninjaBabySelector() {
-  return $('#')
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul pic-list:first-child')
 }
